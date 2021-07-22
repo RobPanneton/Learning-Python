73797055ae94d6734143cad0381d24d74602ab2e
RobPanneton <robert.panneton@gmail.com>
2021-07-22 19:48:26 -0400

    add booleans

---
+++ functions.py
@@ -0,0 +1,19 @@
+def say_hi(user):
+    print("Hello " + user)
+# indentation is important here, it shows  what is inside the function call
+
+
+say_hi("Rob")
+
+
+def cube(num):
+    return num*num*num
+
+
+result = cube(4)
+print(cube(3))
+print(result)
+
+
+
+
---
+++ tuples.py
@@ -0,0 +1,4 @@
+coordinates = (3, 4)
+print(coordinates[0])
+
+# tuples are an immutable datatype
